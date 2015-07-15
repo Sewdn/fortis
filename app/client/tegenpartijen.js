12d19d08f47da6b013e9b892c6b80e715b4060b6
Pieter Soudan <pieter.soudan@redandivory.be>
2015-07-16 00:03:03 +0200

    tegenpartijen

---
+++ app/client/tegenpartijen.js
@@ -0,0 +1,53 @@
+Template.tegenpartijen.onCreated(function(){
+  var self = this;
+  this.selected = new ReactiveVar();
+  this.summary = new ReactiveVar();
+  this.autorun(function(){
+    var tp = self.selected.get();
+    self.subscribe('tegenpartijen');
+    if(tp) {
+      Meteor.call('summary',
+        tp,
+      function(err, data) {
+        self.summary.set(data);
+      });
+    }
+  });
+});
+
+Template.tegenpartijen.onRendered(function(){
+  var self = this;
+});
+
+Template.tegenpartijen.helpers({
+  'all': function(){
+    return Tegenpartijen.find({});
+  },
+  'eur': function(amount) {
+    return "€"+amount.toFixed(2);
+  },
+  'dateFormat': function(date) {
+    //return date.toString();
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+    return [day, monthIndex+1, year].join('/');
+  },
+  isSelected: function() {
+    var s = Template.instance().selected.get();
+    return s && s === this._id._str ? 'is-selected': '';
+  },
+  selected: function() {
+    var s = Template.instance().selected.get();
+    return s && Tegenpartijen.findOne(new Mongo.ObjectID(s));
+  },
+  summary: function() {
+    return Template.instance().summary.get();
+  }
+});
+
+Template.tegenpartijen.events({
+  "click tr": function(event, template){
+    template.selected.set(this._id._str);
+  }
+});

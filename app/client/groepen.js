c32fcfc3fba1ce67a1a1c5da8b468d3bb7a231c7
Pieter Soudan <pieter.soudan@redandivory.be>
2015-07-18 13:08:19 +0200

    summary for groups

--- app/client/groepen.js
+++ app/client/groepen.js
@@ -1,6 +1,18 @@
 Template.groepen.onCreated(function(){
   var self = this;
   this.selected = new ReactiveVar();
+  this.summary = new ReactiveVar();
+  this.autorun(function(){
+    var group = self.selected.get();
+    if(group) {
+      Meteor.call('summary',
+        null,
+        group,
+      function(err, data) {
+        self.summary.set(data);
+      });
+    }
+  });
 });
 
 Template.groepen.helpers({
@@ -14,6 +26,19 @@ Template.groepen.helpers({
   selected: function() {
     var s = Template.instance().selected.get();
     return s && Groepen.findOne(new Mongo.ObjectID(s));
+  },
+  'eur': function(amount) {
+    return "€"+amount.toFixed(2);
+  },
+  'dateFormat': function(date) {
+    //return date.toString();
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+    return [day, monthIndex+1, year].join('/');
+  },
+  summary: function() {
+    return Template.instance().summary.get();
   }
 });
 
